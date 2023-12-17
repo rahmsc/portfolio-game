1bfa9a8906a1ec929383515b30c3e61ae0b32f92
rahmsc <danmilad.code@gmail.com>
2023-12-17 13:09:16 +0700

    Movement and Gravity

--- index.js
+++ index.js
@@ -4,5 +4,82 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+const gravity = 0.5;
+
+class Player {
+  constructor(position) {
+    this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.height = 100;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 100, 100, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y < canvas.height)
+      this.velocity.y += gravity;
+    else this.velocity.y = 0;
+  }
+}
+
+const player = new Player({
+  x: 50,
+  y: 0,
+});
+
+const keys = {
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+};
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  player.velocity.x = 0;
+  if (keys.d.pressed) player.velocity.x = 5;
+  else if (keys.a.pressed) player.velocity.x = -5;
+}
+
+animate();
+
+window.addEventListener("keydown", (event) => {
+  console.log(event);
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "w":
+      player.velocity.y = -15;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  console.log(event);
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+});
